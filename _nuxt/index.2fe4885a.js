69e6ea0c02f36e57395a325583f387775d97bbb4
nwingt <nwingt@users.noreply.github.com>
2023-11-07 02:36:45 +0000

    deploy: 1652ff8bef9783964102bc1d8931b0105643d65e

---
+++ _nuxt/index.2fe4885a.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.63631b5e.js";const o={};function r(t,s){return n(),c("div",null," Welcome! ")}const a=e(o,[["render",r]]);export{a as default};
--- authz/_payload.js
+++
@@ -1,1 +0,0 @@
-export default {data:{},prerenderedAt:1699248222891}
