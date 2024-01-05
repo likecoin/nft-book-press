5a175334c6deb7f2cdaacc3066fa6b95befce035
AuroraHuang22 <aurora90376@gmail.com>
2024-01-05 12:38:41 +0800

    🚚 Move reusable UI & logic to Modal

--- components/ShippingRateModal.vue
+++ components/ShippingRateModal.vue
@@ -13,12 +13,76 @@
         </div>
       </template>
 
+      <component
+        :is="hasMultipleShippingRates ? 'ul' : 'div'"
+        v-if="shippingRates.length"
+        class="space-y-4"
+      >
+        <component
+          :is="hasMultipleShippingRates ? 'li' : 'div'"
+          v-for="(s, index) in shippingRates"
+          :key="s.index"
+        >
+          <UCard
+            :ui="{
+              body: { base: 'space-y-4' },
+              footer: { base: 'flex justify-end items-center' },
+            }"
+          >
+            <UFormGroup
+              label="Name of this shipping option"
+              :ui="{ container: 'space-y-2' }"
+            >
+              <UInput
+                :value="s.nameEn"
+                placeholder="Shipping option name"
+                @input="(e) => updateShippingRate(e, 'nameEn', index)"
+              />
+              <UInput
+                placeholder="運送選項名稱"
+                :value="s.nameZh"
+                @input="(e) => updateShippingRate(e, 'nameZh', index)"
+              />
+            </UFormGroup>
+
+            <UFormGroup label="Price(USD) of this shipping option">
+              <UInput
+                :value="s.price"
+                type="number"
+                step="0.01"
+                :min="0"
+                @input="(e) => updateShippingRate(e, 'price', index)"
+              />
+            </UFormGroup>
+
+            <template v-if="hasMultipleShippingRates" #footer>
+              <UButton
+                label="Delete"
+                variant="outline"
+                color="red"
+                @click="deleteShippingRate(index)"
+              />
+            </template>
+          </UCard>
+        </component>
+        <UButton
+          label="Add Option"
+          variant="outline"
+          @click="addMoreShippingRate"
+        />
+      </component>
+      <div v-else class="flex justify-center items-center py-[36px]">
+        No items
+        <UButton
+          label="Add Option"
+          variant="outline"
+          @click="addMoreShippingRate"
+        />
+      </div>
+
       <template v-if="isEditMode" #footer>
         <div class="flex justify-end items-center">
-          <UButton
-            label="Save"
-            @click="closeModal"
-          />
+          <UButton label="Save" @click="closeModal" />
         </div>
       </template>
     </UCard>
@@ -30,7 +94,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 definePageMeta({ layout: 'page' })
 
-type ModeType = 'edit' | 'view'
+type ModeType = 'edit' | 'view';
 const props = defineProps({
   modelValue: {
     type: Boolean
@@ -46,18 +110,18 @@ const props = defineProps({
 })
 const emit = defineEmits(['update:modelValue'])
 const isEditMode = computed(() => !!(props.mode === 'edit'))
-const modalTitle = computed(() => isEditMode.value ? 'Editing Shipping Options' : 'Shipping Options Info')
-const shippingRates = ref<any[]>(props.shippingInfo.length
-  ? props.shippingInfo.map((option:any) => ({
-    price: option.priceInDecimal / 100,
-    nameEn: option.name?.en,
-    nameZh: option.name?.zh
-  }))
-  : [{
-      price: 10,
-      nameEn: 'Standard Shipping',
-      nameZh: '標準寄送'
-    }])
+const modalTitle = computed(() =>
+  isEditMode.value ? 'Editing Shipping Options' : 'Shipping Options Info'
+)
+const shippingRates = ref<any[]>(
+  props.shippingInfo.length
+    ? props.shippingInfo.map((option: any) => ({
+      price: option.priceInDecimal / 100,
+      nameEn: option.name?.en,
+      nameZh: option.name?.zh
+    }))
+    : []
+)
 const hasMultipleShippingRates = computed(() => shippingRates.value.length > 1)
 
 function closeModal () {
@@ -69,6 +133,15 @@ function updateShippingRate (e: InputEvent, key: string, index: number) {
 }
 
 function addMoreShippingRate () {
+  if (!shippingRates.value.length) {
+    shippingRates.value.push({
+      index: uuidv4(),
+      price: 10,
+      nameEn: 'Standard Shipping',
+      nameZh: '標準寄送'
+    })
+    return
+  }
   shippingRates.value.push({
     index: uuidv4(),
     price: 20,
