bc6e81df95eed733f22d098f8382cf90727c1728
AuroraHuang22 <AuroraHuang22@users.noreply.github.com>
2024-08-14 09:30:17 +0000

    deploy: 6415c4e9de954f663c0d20910f391b8e883e5d69

---
+++ _nuxt/page.b34c6756.js
@@ -0,0 +1,1 @@
+import{_ as o,q as n,w as s,E as a,o as c,L as _}from"./entry.7ed94a47.js";const r={};function l(e,f){const t=a;return c(),n(t,{class:"w-full py-4"},{default:s(()=>[_(e.$slots,"default")]),_:3})}const u=o(r,[["render",l]]);export{u as default};
