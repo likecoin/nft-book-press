389f3c999010c58712d31aa266063ca0b5c732c8
AuroraHuang22 <aurora90376@gmail.com>
2024-08-14 21:09:27 +0800

    🐛 Fix missing signer issue during redirect for AuthCore

--- pages/auth/redirect.vue
+++ pages/auth/redirect.vue
@@ -8,10 +8,18 @@
   </div>
 </template>
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import { useWalletStore } from '~/stores/wallet'
+import { useBookStoreApiStore } from '~/stores/book-store-api'
 
 const store = useWalletStore()
 const { handleConnectorRedirect } = store
+
+const { wallet, signer } = storeToRefs(store)
+const { signMessageMemo } = store
+const bookStoreApiStore = useBookStoreApiStore()
+const { authenticate } = bookStoreApiStore
+const { token, wallet: sessionWallet } = storeToRefs(bookStoreApiStore)
 
 const route = useRoute()
 const router = useRouter()
@@ -23,8 +31,22 @@ onMounted(async () => {
       method: method as string,
       params: { code }
     })
+
+    if (signer.value) {
+      const signature = await signMessageMemo(
+        'authorize',
+        ['read:nftbook', 'write:nftbook', 'read:nftcollection', 'write:nftcollection']
+      )
+      if (!signature) { return }
+      await authenticate(wallet.value, signature)
+      try {
+        window.localStorage.setItem('likecoin_nft_book_press_token', JSON.stringify({ wallet: sessionWallet.value, token: token.value }))
+      } catch (err) {}
+    }
+
     const postAuthRoute = '/'
     router.replace(postAuthRoute)
   }
 })
+
 </script>
