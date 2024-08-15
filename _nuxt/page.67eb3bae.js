d3e7cc98cb40d9c21b0843972aedd01e2d70cc80
williamchong <williamchong@users.noreply.github.com>
2024-08-15 08:38:55 +0000

    deploy: beaa1de478db95da444f5d3b075d5b8c5f21c948

---
+++ _nuxt/page.67eb3bae.js
@@ -0,0 +1,1 @@
+import{_ as o,x as n,w as s,H as a,o as c,N as _}from"./entry.9d9444cd.js";const r={};function l(e,f){const t=a;return c(),n(t,{class:"w-full py-4"},{default:s(()=>[_(e.$slots,"default")]),_:3})}const u=o(r,[["render",l]]);export{u as default};
