69cb17e5d79e8d8e9ac2d6cd7ecfe48c6d2f5468
Ng Wing Tat, David <i@ngwingt.at>
2024-08-29 11:09:38 +0800

    🔧 Use runtime config for constant

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,33 @@ export default defineNuxtConfig({
       }
     }
   },
+  runtimeConfig: {
+    public: {
+      IS_TESTNET: process.env.IS_TESTNET || 'TRUE',
+
+      SITE_URL: process.env.SITE_URL || 'https://likecoin-nft-book-press-testnet.netlify.app',
+
+      CHAIN_ID: process.env.CHAIN_ID || 'likecoin-public-testnet-5',
+      CHAIN_NAME: process.env.CHAIN_NAME || 'LikeCoin public test chain',
+      CHAIN_DENOM: process.env.CHAIN_DENOM || 'EKIL',
+      CHAIN_MINIMAL_DENOM: process.env.CHAIN_MINIMAL_DENOM || 'nanoekil',
+      COINGECKO_ID: process.env.COINGECKO_ID || undefined,
+      RPC_URL: process.env.RPC_URL || 'https://node.testnet.like.co/rpc/',
+      LCD_URL: process.env.LCD_URL || 'https://node.testnet.like.co',
+      CHAIN_EXPLORER_URL: process.env.CHAIN_EXPLORER_URL || 'https://node.testnet.like.co/cosmos/tx/v1beta1/txs',
+
+      LIKER_NFT_FEE_WALLET: process.env.LIKER_NFT_FEE_WALLET || 'like1yney2cqn5qdrlc50yr5l53898ufdhxafqz9gxp',
+      LIKER_NFT_TARGET_ADDRESS: process.env.LIKER_NFT_TARGET_ADDRESS || 'like1yney2cqn5qdrlc50yr5l53898ufdhxafqz9gxp',
+
+      AUTHCORE_API_HOST: process.env.AUTHCORE_API_HOST || 'https://likecoin-integration-test.authcore.io',
+      LIKER_LAND_URL: process.env.LIKER_LAND_URL || 'https://rinkeby.liker.land',
+      LIKE_CO_HOST: process.env.LIKE_CO_HOST || 'https://rinkeby.like.co',
+      LIKE_CO_API: process.env.LIKE_CO_API || 'https://api.rinkeby.like.co',
+      APP_LIKE_CO_URL: process.env.APP_LIKE_CO_URL || 'https://app.rinkeby.like.co',
+      NFT_MARKETPLACE_URL: process.env.NFT_MARKETPLACE_URL || 'https://likecoin-nft-marketplace-testnet.netlify.app',
+      ISCN_TOOLS_URL: process.env.ISCN_TOOLS_URL || 'https://likecoin-iscn-nft-tools-testnet.netlify.app'
+    }
+  },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'page', mode: 'out-in' }
