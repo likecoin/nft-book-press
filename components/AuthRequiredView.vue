938e5fbafa2f6f2863dc9e73df7a11df4847c161
Ng Wing Tat, David <i@ngwingt.at>
2024-08-31 23:47:44 +0800

    🚸 Unify login/connect wallet UI/UX

---
+++ components/AuthRequiredView.vue
@@ -0,0 +1,27 @@
+<template>
+  <PageBody
+    v-if="!wallet || !bookStoreApiStore.isAuthenticated"
+    :ui="{ base: 'flex justify-center items-center grow' }"
+  >
+    <UAlert
+      icon="i-heroicons-light-bulb"
+      color="amber"
+      variant="subtle"
+      title="Sign in required"
+      description="Please sign in to continue."
+      :ui="{ wrapper: 'w-auto', inner: 'w-auto', title: 'font-bold' }"
+    />
+  </PageBody>
+  <slot v-else />
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
+import { useBookStoreApiStore } from '~/stores/book-store-api'
+import { useWalletStore } from '~/stores/wallet'
+
+const bookStoreApiStore = useBookStoreApiStore()
+const store = useWalletStore()
+const { wallet } = storeToRefs(store)
+</script>
