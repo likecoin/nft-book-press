335367f99718e2ba99967b50dbcf18b7eb5d3333
AuroraHuang22 <aurora90376@gmail.com>
2025-03-05 00:06:54 +0800

    💬 Rename IRYS_URL to IRYS_NODE_URL

--- utils/arweave.ts
+++ utils/arweave.ts
@@ -125,9 +125,9 @@ async function getBundler ({
     WebBundlr = (await import('@bundlr-network/client')).default
   }
   const p = await getProvider({ fileSize, ipfsHash, txHash, token })
-  const { IRYS_URL } = useRuntimeConfig().public
+  const { IRYS_NODE_URL } = useRuntimeConfig().public
   const bundlr = new WebBundlr(
-    IRYS_URL,
+    IRYS_NODE_URL,
     'matic',
     p
   )
