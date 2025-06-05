daf8e4bde4313a50705bfd6989a6584e633df9de
AuroraHuang22 <aurora90376@gmail.com>
2025-06-05 11:02:06 +0800

    🔥 Remove unused fileToBase64 function

--- utils/uploadFile.ts
+++ utils/uploadFile.ts
@@ -52,12 +52,3 @@ export function clearUploadFileData () {
     console.warn('Failed to clear sessionStorage:', error)
   }
 }
-
-export function fileToBase64 (file: File): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () => resolve(reader.result as string)
-    reader.onerror = error => reject(error)
-  })
-}
