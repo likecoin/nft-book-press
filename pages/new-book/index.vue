3635484b6beb005a2de2b6370e0834378016f1ae
AuroraHuang22 <aurora90376@gmail.com>
2025-08-27 10:12:54 +0800

    💬 Rename /publish-nft-book to /new-book
    (cherry picked from commit 0deb689f6681114fc82fc7a188bc3a63e50e94b7)
    
    💬 Update copywriting for file upload step
    
    (cherry picked from commit 90bd713045d9a4b22c6d9de1116fd1bf854b84b6)
    
    💬 Update copywriting for metadata registration
    
    (cherry picked from commit dd981abf67e94710bc1ddc096ee7e54e5a252d5a)

--- pages/new-book/index.vue
+++ pages/new-book/index.vue
@@ -59,45 +59,9 @@
           <UButton
             v-if="shouldShowActionButton"
             :disabled="shouldDisableAction"
+            :label="currentActionText"
             @click="nextStep"
-          >
-            {{ currentActionText }}
-          </UButton>
-        </div>
-      </div>
-      <div v-if="step === 0" class="flex flex-col justify-center px-[12px] mt-[16px]">
-        <div class="w-full bg-gray-300 h-[1px]" />
-        <div class="flex flex-col items-center gap-4 py-4">
-          <div class="flex flex-col items-center">
-            <div v-if="hasExistingSessionData">
-              {{ $t('publish_steps.continue_last_register') }}
-              <UButton variant="ghost" class="text-primary-500 font-semibold" @click="step = 1">
-                {{ bookName }}
-              </UButton>
-            </div>
-            <div class="flex items-center">
-              <span>{{ $t('publish_steps.already_have_iscn') }}</span>
-              <UButton
-                variant="ghost"
-                @click="showIscnInput = !showIscnInput"
-              >
-                {{ $t('publish_steps.here') }}
-              </UButton>
-            </div>
-          </div>
-
-          <div v-if="showIscnInput" class="flex flex-col items-center gap-2 w-full max-w-md">
-            <UInput
-              v-model="iscnInputValue"
-              :placeholder="$t('publish_steps.enter_iscn_id')"
-            />
-            <UButton
-              :disabled="!iscnInputValue"
-              @click="handleIscnInput"
-            >
-              {{ $t('common.confirm') }}
-            </UButton>
-          </div>
+          />
         </div>
       </div>
     </div>
@@ -122,7 +86,6 @@ const step = ref(0)
 const uploadFormRef = ref()
 const registerISCN = ref()
 const mintNFT = ref()
-const showIscnInput = ref(false)
 const iscnInputValue = ref('')
 const bookName = ref('')
 
@@ -135,19 +98,16 @@ const isISCNFormValid = ref(false)
 const isMintFormValid = ref(false)
 const isMintLoading = ref(false)
 
-const hasExistingSessionData = computed(() => {
-  return !!bookName.value
-})
 const currentActionText = computed(() => {
   switch (step.value) {
     case 0:
-      return 'Start Upload'
+      return $t('publish_button.upload_files')
     case 1:
-      return 'Register ISCN'
+      return $t('publish_button.metadata')
     case 2:
-      return 'Mint NFT'
+      return $t('pages.next')
     default:
-      return 'Next'
+      return $t('publish_button.publish_now')
   }
 })
 
@@ -278,12 +238,6 @@ const handleMintNFTSubmit = async (res: any) => {
 
 const handleNewBookSubmit = async () => {
   await navigateTo(localeRoute({ name: 'nft-book-store' }))
-}
-
-const handleIscnInput = async () => {
-  if (iscnInputValue.value) {
-    await handleIscnSubmit({ iscnId: iscnInputValue.value, txHash: '' })
-  }
 }
 
 </script>
