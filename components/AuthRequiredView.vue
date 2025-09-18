209440088f4939ca88d2a2ea63981e0dea9c31ee
AuroraHuang22 <aurora90376@gmail.com>
2025-09-18 14:21:38 +0800

    🚸 Add loading text
    🎨 Remove wallet check

--- components/AuthRequiredView.vue
+++ components/AuthRequiredView.vue
@@ -26,7 +26,7 @@
       <div class="flex justify-center w-[120px]">
         <UButton
           :ui="{base:'!bg-[#50E3C2] !text-black hover:!bg-[#40caa8]'}"
-          :label="$t('auth_state.login')"
+          :label="isRestoringSession ? $t('auth_state.restore') : $t('auth_state.login')"
           size="lg"
           block
           :loading="isAuthenticating"
